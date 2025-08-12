840abe9b025d5e2a4b9aef846d2d3b2845009c58
harish khileri <harishkhileri98@gmail.com>
2025-08-12 11:51:05 +0530

    using if else

---
+++ if-elif-else.py
@@ -0,0 +1,17 @@
+#In this file we are get over grade from percentage
+percentage = int(input("Enter your percentage : "))
+if percentage >= 90:
+    print("Your grade is : Grade A+")
+    print("You are in over toper list")
+elif percentage >= 80:
+    print("Your grade is : Grade A")
+elif percentage >= 70:
+    print("Your grade is : Grade B")
+elif percentage >= 60:
+    print("Your grade is : Grade C")
+elif percentage >= 50:
+    print("Your grade is : Grade D")
+else:
+    print("Fail")
+    print("sorry you are not pripraing well")
+    print("try again if you want")
