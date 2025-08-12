7a1c1ddb69d93cd35d9332a649cbc3116ac9c8e8
harish khileri <harishkhileri98@gmail.com>
2025-08-12 12:54:52 +0530

    how to print keyword list in python using kwlist

--- Comment.py
+++ Comment.py
@@ -19,3 +19,9 @@ yr
 u
 tur 
 comment '''
+#printing the list of keyword using kwlist
+# import keyword
+# print("The list of all keyword's in python")
+# print(keyword.kwlist)
+import keyword
+print(keyword.kwlist)
