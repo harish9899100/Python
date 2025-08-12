88741ebdf0fb00ddb1efbbda4563462baa6d4cd5
harish khileri <harishkhileri98@gmail.com>
2025-08-12 10:24:42 +0530

    add new file

---
+++ comment.py
@@ -0,0 +1,21 @@
+print("In python language are persent two type of comment ")
+# This is single line comment
+""" This is
+fsf
+sd
+fsfs
+fsd
+fs
+dfsf
+f
+sfs
+multyline comment """
+''' this is alos multyline
+wer
+w
+er
+yrt
+yr
+u
+tur 
+comment '''
