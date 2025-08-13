854075665a7e367ce8f8ed3b3c97f25cefead065
harish khileri <harishkhileri98@gmail.com>
2025-08-13 21:48:48 +0530

    how to work Nested if in python

--- Nested-if-1.py
+++ Nested-if-1.py
@@ -1,9 +1,21 @@
-age = int(input("Enter your age : "))
-mamber = str(input("If you are member of this company then enter yes : \nOther-wise press enter : "))
+# age = int(input("Enter your age : "))
+# mamber = bool(input("If you are member of this company then enter yes : \nOther-wise press enter : "))
+# if age >= 60:
+#     if mamber == True:
+#         print("Your senior discount is : 40% ")
+#     else:
+#         print("Your senior discount is : 25% ")
+# else:
+#     print("Yor are not eligible for any discount.")
+
+
+
+age = int(input("Enter your age :"))
+mamber = input("Enter True if you are bember of this company :\nOtherwise press Enter :")
 if age >= 60:
-    if mamber == True:
-        print("Your senior discount is : 40% ")
+    if mamber.lower() == "yes":
+        print("Your seniour discount is : 40% ")
     else:
-        print("Your senior discount is : 25% ")
+        print("Your seniour discount is : 25% ")
 else:
-    print("Yor are not eligible for any discount.")
+    ("You are not eligible for any type's of discount.")
