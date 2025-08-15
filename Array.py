54998b7dd4a076e8486eb01c4d693dd3e41369e2
harish khileri <harishkhileri98@gmail.com>
2025-08-15 14:43:12 +0530

    how to work array in python

---
+++ Array.py
@@ -0,0 +1,10 @@
+# In this file we are practice about what is array and how to work array in python
+
+cars = ["mercedes", "bmw", "mustange", "lamborgini"]
+print(cars)
+print(type(cars))
+print(len(cars))
+cars.append("oudi")
+print(cars)
+cars.sort()
+print(cars)
