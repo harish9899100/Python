1a4b5b4a6566a8154c535d0834a5023e16b172c3
harish khileri <harishkhileri98@gmail.com>
2025-08-16 09:13:38 +0530

    The perfect way for claculating electricity bill

--- Electricity_Bill.py
+++ Electricity_Bill.py
@@ -1,15 +1,15 @@
 # In This Program we are check how to calculate electricity bill 
 unit = int(input("Enter Your Electricity Units : "))
-max=100
+min=100
 bill=0
 if unit <= 50:
-    bill=max
+    bill=min
 elif unit > 50 and unit <= 100:
-    bill=max + (unit-50)*2.0
+    bill=min + (unit-50)*2.0
 elif unit > 100 and unit <= 200:
-    bill=max + 50*2 + (unit-100)*3.0
+    bill=min + 50*2 + (unit-100)*3.0
 elif unit > 200 and unit <= 300:
-    bill= max + 50*2 + 100*3 + (unit-200)*3.5
+    bill= min + 50*2 + 100*3 + (unit-200)*3.5
 else:
-    bill= max + 50*2 + 100*3 + 100*3.5 + (unit-300)*5
+    bill= min + 50*2 + 100*3 + 100*3.5 + (unit-300)*5
 print("Your Electricity Units Is : ",unit,"And Your Electcricity Bill Is : Rs.",bill)
