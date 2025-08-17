b9996e87f273055f5c8efc1345a1dccf32f3ec23
harish khileri <harishkhileri98@gmail.com>
2025-08-17 10:32:28 +0530

    pythons practice

---
+++ Inheritance.py
@@ -0,0 +1,28 @@
+# class animal:
+#     def speck(self):
+#         print("This animal are make sound :")
+# class dog(animal):
+#     def speck(self):
+#         print("dogs sound like huf-fuw")
+# class cat(animal):
+#     def speck(self):
+#         print("cat sound like meao - meao")
+# a = animal()
+# a.speck()
+# b = dog()
+# b.speck()
+# c = cat()
+# c.speck()
+
+
+class student:
+    def name(self):
+        print("The name of students is :")
+    def harish(name):
+        print("The student name is harish")
+    def raj(name):
+        print("The student name is raj")
+a = student()
+a.harish()
+b = student()
+b.raj()
---
+++ Math.py
@@ -0,0 +1,23 @@
+# a = min(2,34,46,7,8,9,)
+# b = max(23,34,5,6,8,89)
+# print(a)
+# print(b)
+
+# num = abs(-34)
+# print(num)
+
+# x = pow(4, 2)
+# print(x)
+
+
+# import math
+# x = math.sqrt(64)
+# print(x)
+
+import math
+x = math.ceil(5.3)
+print(x)
+y = math.floor(5.6)
+print(y)
+c = math.pi
+print(c)
