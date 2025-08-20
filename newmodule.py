1b1ebc77a666e8f1221c4fa7e7c5f77da203f235
harish khileri <harishkhileri98@gmail.com>
2025-08-20 14:36:27 +0530

    create .gitignore file

---
+++ newmodule.py
@@ -0,0 +1,16 @@
+# import math
+# num = int(input("Enter a number for find squre : "))
+# num1 = math.sqrt(num)
+# print(num1)
+
+# def add(a, b):
+#     return a + b
+# a = add(3,5)
+# print(a)
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n-1)
+a = factorial(int(input("Enter a number for getting factorial : ")))
+print(f"Your numbers factorial is :{a}")
